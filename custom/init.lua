9f161bd2e4de25897cfd1d8e95e000fcee6388c2
ChenKS12138 <chenks12138@gmail.com>
2024-09-17 23:39:55 +0800

    feat: wsl support

--- custom/init.lua
+++ custom/init.lua
@@ -21,3 +21,18 @@ vim.api.nvim_create_user_command("Format", function(args)
 end, { range = true })
 
 
+if vim.fn.has("wsl") == 1 then
+  vim.g.clipboard = {
+    name = "win32yank-wsl",
+    copy = {
+      ["+"] = "win32yank.exe -i --crlf",
+      ["*"] = "win32yank.exe -i --crlf",
+    },
+    paste = {
+      ["+"] = "win32yank.exe -o --lf",
+      ["*"] = "win32yank.exe -o --lf",
+    },
+    cache_enabled = 0,
+  }
+end
+
